160aaf335dcbf97d6645dd9a41b3447ec1b85d6f
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-08-30 12:17:35 +0600

    File added

---
+++ compare.js
@@ -0,0 +1,28 @@
+/*
+*COMPARISON
+
+bigger --> >
+less --> <
+equal --> ==
+gater than or equal --> >=
+less than or equal --> <=
+not equal --> !=
+
+----------------------------
+and --> &&
+or --> ||
+*/
+
+// console.log(5 < 10);
+// console.log(5 > 10);
+// console.log(5 == 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+
+// console.log(10 == 10);
+// console.log(10 == "10");
+// console.log(10 === "10"); //3 equal use korle type ta ki shera check kore
+
+console.log(10 != "10");
+console.log(10 !== "10");
+console.log(10 !== 9);
---
+++ variable.js
@@ -0,0 +1,12 @@
+/*
+ * VARIABLE ==> Container
+ */
+
+// Number
+var sunglassPrice = 80;
+
+// String
+var subject = "Bangla";
+
+// Boolean
+var passed = true;
